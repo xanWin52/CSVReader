f948ba55e49379bc8182b6fb8f7860c1dfe9ea6d
Xander Nguyen <77035811+xanWin52@users.noreply.github.com>
2023-08-22 14:13:45 -0500

    Starting again

--- AttendanceUpdater.java
+++ AttendanceUpdater.java
@@ -1,8 +1,19 @@
 import java.io.FileReader; 
+import java.io.FileWriter;
 import java.io.IOException; 
+import java.io.BufferedReader;
+import java.io.File;
+import java.util.ArrayList;
 
 public class AttendanceUpdater {
     public static void main(String[] args) throws IOException{
-        
+        BufferedReader signIn = new BufferedReader(new FileReader("Test Event Sign In.csv"));
+        BufferedReader attendance = new BufferedReader(new FileReader("Test Attendance.csv"));
+        String line;
+        ArrayList<String[]> newAtt = new ArrayList<>();
+        while((line = attendance.readLine())!= null){
+            newAtt.add(line.split(","));
+        }
+        FileWriter result = new FileWriter(new File("Result.csv"));
     }    
 }
