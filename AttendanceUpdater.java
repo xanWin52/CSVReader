d1c13e7c3ac9e1ec30a1b601d3255635c2f12f25
Xander Nguyen <77035811+xanWin52@users.noreply.github.com>
2023-08-25 12:53:42 -0500

    Fix addEvent error

--- AttendanceUpdater.java
+++ AttendanceUpdater.java
@@ -2,6 +2,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException; 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -9,31 +10,34 @@ import java.util.List;
 
 public class AttendanceUpdater {
     public static void main(String[] args) throws IOException{
+
         BufferedReader attendance = new BufferedReader(new FileReader("Test Attendance.csv"));
+        BufferedWriter result = new BufferedWriter(new FileWriter("Result.csv"));
+
         String line;
         ArrayList<List<String>> newAtt = new ArrayList<List<String>>();
         while((line = attendance.readLine())!= null){
-            System.out.println(line);
-            newAtt.add(Arrays.asList(line.split(",")));
+            ArrayList<String> lineTemp = new ArrayList<>(Arrays.asList(line.split(",")));
+            newAtt.add(lineTemp);
         }
         newAtt = addEvent(newAtt,"Test");
-        FileWriter result = new FileWriter(new File("Result.csv"));
         for(List<String> x : newAtt){
             result.write(x.toString());
+            result.write("\n");
         }
         attendance.close();
+        result.close();
     }    
 
     public static ArrayList<List<String>> addEvent(ArrayList<List<String>> att, String title)throws IOException{
         BufferedReader signIn = new BufferedReader(new FileReader("Test Event Sign In.csv"));
+        att.get(1).add(title);
         String line;
         signIn.readLine();
         while((line = signIn.readLine()) != null){
             String[] entry = line.split(",");
             for(int i = 5; i < att.size(); i ++){
-                if((att.get(i).get(0).equals(entry[2]) && att.get(i).get(1).equals(entry[1])) || (att.get(i).get(0).equals(entry[1]) && att.get(i).get(1).equals(entry[2]))){
-                    att.get(i).add(entry[0]);
-                }
+                att.get(i).add("Test");
             }
         }
         signIn.close();
